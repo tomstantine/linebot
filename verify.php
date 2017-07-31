273a6e91c0e310afbb016ef735fd6dd41e6989fc
siwapong <tomcs19@gmail.com>
2017-07-31 21:58:33 +0700

    update token

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'I7My0/9QXevs6mCqXPe+JuejCrLmp6TuYRQSGh5o4r1l62F04OMc8csVbJOcn/y68vqmQKkcbjtA3M1R0a8wBGYOag4TQSPKNt2pUlSutwj/sYNvrV4pW6p+ttbh9xNbvEiKjU7bo8j2Dd4iYebJKQdB04t89/1O/w1cDnyilFU=';
+$access_token = 'iyzxmENf2gQ0OqPLPnqRCLhtiuQOJU/q/APRq1OhUA9V/UPF0gksiq5b4PPFfUic8vqmQKkcbjtA3M1R0a8wBGYOag4TQSPKNt2pUlSutwh7WX5rrququKSiQjRa7OCGR07u5e9xx9VrUrMxdAWcdwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
