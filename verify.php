73fdd1a5357d0265f2dc13b663a066b421401365
siwapong <tomcs19@gmail.com>
2017-08-01 00:51:16 +0700

    update txt

--- verify.php
+++ verify.php
@@ -1,12 +1,12 @@
 <?php
-$access_token = 'iyzxmENf2gQ0OqPLPnqRCLhtiuQOJU/q/APRq1OhUA9V/UPF0gksiq5b4PPFfUic8vqmQKkcbjtA3M1R0a8wBGYOag4TQSPKNt2pUlSutwh7WX5rrququKSiQjRa7OCGR07u5e9xx9VrUrMxdAWcdwdB04t89/1O/w1cDnyilFU=';
+$access_token = 'xxx';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
-$proxy = 'proxyurl:http://fixie:QzdR7a5mSBt7FCC@velodrome.usefixie.com:80';
-$proxyauth = 'tomcs19@gmail.com:he123449';
+$proxy = 'xxx';
+$proxyauth = 'xxx';
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
